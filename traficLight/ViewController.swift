38da5285f3297a86aab531f8270bcac996571a9d
duplin anton <duplin.anton@gmail.com>
2021-07-06 22:28:53 +0300

    Initial Commit

---
+++ traficLight/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  traficLight
+//
+//  Created by Anton Duplin on 6/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
