fae219b6c7389aee8135c95fcd8357a8bde564c8
duplin anton <duplin.anton@gmail.com>
2021-07-08 10:04:51 +0300

    add traficControl without bug

--- traficLight/ViewController.swift
+++ traficLight/ViewController.swift
@@ -11,13 +11,13 @@ enum TraficLights {
     case yellow
     case green
 }
-private var traficLights = TraficLights.red
-
 class ViewController: UIViewController {
     @IBOutlet weak var redColorLabel: UIView!
     @IBOutlet weak var yellowColorLabel: UIView!
     @IBOutlet weak var greenColorLabel: UIView!
     @IBOutlet weak var startButon: UIButton!
+    
+    private var traficLights = TraficLights.red
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,21 +25,20 @@ class ViewController: UIViewController {
         yellowColorLabel.layer.cornerRadius = 100
         greenColorLabel.layer.cornerRadius = 100
     }
-    
     @IBAction func startToNext() {
         startButon.setTitle("Next", for: .normal)
         switch traficLights {
         case .red:
             redColorLabel.alpha = 1
-            redColorLabel.alpha = 0.3
+            greenColorLabel.alpha = 0.3
             traficLights = .yellow
         case .yellow:
-            yellowColorLabel.alpha = 0.3
+            redColorLabel.alpha = 0.3
             yellowColorLabel.alpha = 1
             traficLights = .green
         case .green:
-            greenColorLabel.alpha = 0.3
-            yellowColorLabel.alpha = 1
+            yellowColorLabel.alpha = 0.3
+            greenColorLabel.alpha = 1
             traficLights = .red
         }
     }
